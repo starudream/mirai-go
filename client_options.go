80b703d56ed87ba9b66b5c8a44622d6a9f000cc8
starudream <justwangsheng@gmail.com>
2023-03-31 17:24:42 +0800

    feat: add client options

--- client_options.go
+++ client_options.go
@@ -1,6 +1,17 @@
 package mirai
 
+import (
+	"github.com/go-resty/resty/v2"
+)
+
 type ClientOptionFunc func(c *Client) error
+
+func WithClient(client *resty.Client) ClientOptionFunc {
+	return func(c *Client) error {
+		c.client = client
+		return nil
+	}
+}
 
 func WithUserAgent(userAgent string) ClientOptionFunc {
 	return func(c *Client) error {

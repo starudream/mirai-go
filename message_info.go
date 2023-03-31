173384a4c912df9242d049c451ae8684fef0ebe8
starudream <justwangsheng@gmail.com>
2023-03-31 21:12:41 +0800

    feat: add message info

--- message_info.go
+++ message_info.go
@@ -28,6 +28,28 @@ type MessageInfoInterface interface {
 	setType()
 }
 
+type MessageInfoSource struct {
+	Type MessageInfoType `json:"type"`
+	Id   int64           `json:"id"`
+	Time int64           `json:"time"`
+}
+
+func (m *MessageInfoSource) setType() { m.Type = MessageInfoTypeSource }
+
+type MessageInfoAt struct {
+	Type    MessageInfoType `json:"type"`
+	Target  int64           `json:"target"`
+	Display string          `json:"display,omitempty"`
+}
+
+func (m *MessageInfoAt) setType() { m.Type = MessageInfoTypeAt }
+
+type MessageInfoAtAll struct {
+	Type MessageInfoType `json:"type"`
+}
+
+func (m *MessageInfoAtAll) setType() { m.Type = MessageInfoTypeAtAll }
+
 type MessageInfoPlain struct {
 	Type MessageInfoType `json:"type"`
 	Text string          `json:"text"`
